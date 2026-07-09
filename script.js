5c92f2d2538938e9de999247371dac2ada4bf0d7
ghost <138735754+Ryan20014737472@users.noreply.github.com>
2026-07-09 20:24:39 -0300

    Implement image carousel functionality

--- script.js
+++ script.js
@@ -30,3 +30,105 @@ if (pesquisa) {
     });
 
 }
+const carrossel = document.querySelector(".carrossel");
+
+if(carrossel){
+
+    const principal = document.querySelector(".imagemPrincipal");
+    const thumbs = document.querySelectorAll(".thumb");
+    const esquerda = document.querySelector(".esquerda");
+    const direita = document.querySelector(".direita");
+
+    const imagens = [];
+
+    thumbs.forEach((thumb)=>{
+
+        imagens.push(thumb.src);
+
+    });
+
+    let indice = 0;
+
+    function atualizar(){
+
+        principal.src = imagens[indice];
+
+        thumbs.forEach((thumb)=>{
+
+            thumb.classList.remove("ativa");
+
+        });
+
+        thumbs[indice].classList.add("ativa");
+
+    }
+
+    direita.addEventListener("click",()=>{
+
+        indice++;
+
+        if(indice>=imagens.length){
+
+            indice=0;
+
+        }
+
+        atualizar();
+
+    });
+
+    esquerda.addEventListener("click",()=>{
+
+        indice--;
+
+        if(indice<0){
+
+            indice=imagens.length-1;
+
+        }
+
+        atualizar();
+
+    });
+
+    thumbs.forEach((thumb,i)=>{
+
+        thumb.addEventListener("click",()=>{
+
+            indice=i;
+
+            atualizar();
+
+        });
+
+    });
+
+    let automatico = setInterval(proximaImagem,5000);
+
+    function proximaImagem(){
+
+        indice++;
+
+        if(indice>=imagens.length){
+
+            indice=0;
+
+        }
+
+        atualizar();
+
+    }
+
+    carrossel.addEventListener("mouseenter",()=>{
+
+        clearInterval(automatico);
+
+    });
+
+    carrossel.addEventListener("mouseleave",()=>{
+
+        automatico=setInterval(proximaImagem,5000);
+
+    });
+
+}
